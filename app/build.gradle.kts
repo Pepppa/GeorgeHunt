72b0d165393e5b24fbf0e51ac73800ba7bab89c4
Olga Shiliagina <olg.and.ipf@gmail.com>
2026-03-31 20:41:57 +0200

    Changed package name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,21 +1,22 @@
+import java.io.File
+
 plugins {
     alias(libs.plugins.android.application)
 }
 
+val appVersionName = "1.0"
+
 android {
     namespace = "com.example.georgehunt"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 36
+
 
     defaultConfig {
         applicationId = "com.example.georgehunt"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
+        versionName = appVersionName
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -35,6 +36,8 @@ android {
     }
 }
 
+
+
 dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
@@ -43,4 +46,19 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+}
+
+afterEvaluate {
+    tasks.named("packageRelease") {
+        doLast {
+            val releaseDir = file("${projectDir}/release")
+            releaseDir.listFiles()?.forEach { apkFile ->
+                if (apkFile.name.endsWith(".apk")) {
+                    val newFile = File(releaseDir, "george-hunt-$appVersionName-release.apk")
+                    val result = apkFile.renameTo(newFile)
+                    println(">>> renamed: $result -> ${newFile.name}")
+                }
+            }
+        }
+    }
 }
