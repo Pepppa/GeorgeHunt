04d32e531ad12afb0be13a0fe7fdfbd6cf4209a5
Olga Shiliagina <olg.and.ipf@gmail.com>
2026-04-04 10:05:41 +0200

    Real preview size

--- app/src/main/java/com/example/georgehunt/BallPreviewView.java
+++ app/src/main/java/com/example/georgehunt/BallPreviewView.java
@@ -5,34 +5,42 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.view.View;
 
 public class BallPreviewView extends View {
 
     private Paint paint;
     private int radiusProgress = 50;
+    private float realRadius = 0f;
 
     public BallPreviewView(Context context, AttributeSet attrs) {
         super(context, attrs);
         paint = new Paint();
         paint.setAntiAlias(true);
         paint.setColor(Color.YELLOW);
+        updateRealRadius(context);
     }
 
     public void setRadiusProgress(int progress) {
         this.radiusProgress = progress;
+        updateRealRadius(getContext());
         invalidate();
     }
 
     public float getCurrentRadius() {
-        float screenMin = Math.min(getWidth(), getHeight());
-        return GameSettings.toRadius(radiusProgress, screenMin);
+        return realRadius;
+    }
+
+    private void updateRealRadius(Context context) {
+        DisplayMetrics dm = context.getResources().getDisplayMetrics();
+        float screenMin = Math.min(dm.widthPixels, dm.heightPixels);
+        realRadius = GameSettings.toRadius(radiusProgress, screenMin);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        float radius = getCurrentRadius();
-        canvas.drawCircle(getWidth() / 2f, getHeight() / 2f, radius, paint);
+        canvas.drawCircle(getWidth() / 2f, getHeight() / 2f, realRadius, paint);
     }
 }
